296f10c02ca6156b3ff9c9cb7daa84358a9b35ff
Reza Aghajani <rezaaghajani@protonmail.com>
2022-02-12 19:52:49 +0330

    Make design a little better

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@
   <div id="app">
     <h1>Todo App</h1>
     <form @submit.prevent="addTodo()">
-      <label> New ToDo </label>
       <input type="text" v-model="newTodo" placeholder="Add a new todo">
       <button type="submit">Add</button>
     </form>
 
     <h2>Todo List</h2>
     <ul>
-      <li v-for="(todo,index) in todos" :key="index">
+      <li v-for="(todo,index) in todos" :key="index" :class="{lidone: todo.done}">
         <span :class="{done: todo.done}" @click="doneToDo(todo)">
           {{ todo.content}}
         </span>
@@ -57,12 +56,21 @@ export default {
 </script>
 
 <style>
+body {
+  background: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
 }
 
@@ -70,7 +78,50 @@ export default {
   text-decoration: line-through;
 }
 
+
+
+form input {
+  font-size: 16px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  background: none;
+  color: white;
+}
+
+form input::placeholder {
+  color: #ccc;
+  opacity: 0.8;
+}
+
+form button {
+  background: none;
+  border: 1px solid #ccc;
+  color: white;
+  font-size: 16px;
+  margin: 0px 5px;
+  border-radius: 7px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+form button:hover {
+  background:white;
+  color: #2c3e50;
+}
+
 li {
   cursor: pointer;
+  text-decoration: none;
+  list-style-type: none;
+  width: auto;
+  margin: 7px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.lidone {
+  background: green;
+  color : white;
 }
 </style>
